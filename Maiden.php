d9006c7ed2a33b98da500e3c89821b999d094283
Paul Serby <paul.serby@clock.co.uk>
2011-05-17 17:56:57 +0100

    Adding standard build targets to Maiden.php for this project.

--- Maiden.php
+++ Maiden.php
@@ -30,7 +30,56 @@ class MaidenProject extends MaidenDefault {
 		$this->logger->log("Install complete. Type maiden -h to ensure you have '{$this->symlinkPath}' in your path.");
 	}
 
+	/**
+	 * Looks for messy code using PHPMD
+	 */
 	public function checkForMess() {
 		$this->exec("phpmd lib text codesize,unusedcode,naming,design");
+	}
+
+	/**
+	 * Create mess report
+	 */
+	public function createMessReport() {
+		$this->clean();
+		$this->exec("phpmd lib xml codesize,unusedcode,naming,design --reportfile build/log/pmd.xml");
+	}
+
+	/**
+	 * Runs all the phpunit tests
+	 */
+	public function test() {
+		$this->clean();
+		chdir("test");
+		$this->exec("phpunit --bootstrap PitonBootstrap.php Piton");
+	}
+
+	/**
+	 * Creates phpunit test reports
+	 */
+	public function createTestReports() {
+		$this->clean();
+		chdir("test");
+		$this->exec("phpunit --log-junit ../build/log/phpunit.xml --coverage-clover ../build/coverage/coverage.xml --bootstrap PitonBootstrap.php Piton");
+	}
+
+	/**
+	 * Cleans for build directory and recreates it.
+	 */
+	public function clean() {
+		$this->exec("rm -rf build");
+		mkdir("build/coverage", 0775, true);
+		mkdir("build/log", 0775, true);
+	}
+
+	/**
+	 * Validates all the PHP code with php -l
+	 */
+	public function validate() {
+		$path = ".";
+		$iterator = new \RegexIterator(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path)), "/\.php$/i");
+		foreach ($iterator as $filePath) {
+			$this->exec("php -l {$filePath}");
+		}
 	}
 }
